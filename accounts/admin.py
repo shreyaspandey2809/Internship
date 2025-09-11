6abb01242d298debc2db4acd84c9351fba344c3c
Shreyas Pandey <shreyaspandey69@gmail.com>
2025-09-11 17:31:13 +0530

    project changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from .models import Event, Registration
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 User = get_user_model()
-
-# Register User model
-@admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "email", "is_staff", "is_superuser")
-    search_fields = ("username", "email")
-    list_filter = ("is_staff", "is_superuser")
 
 # Register Event model
 @admin.register(Event)
@@ -24,3 +18,9 @@ class RegistrationAdmin(admin.ModelAdmin):
     list_display = ("id", "student", "event")
     search_fields = ("student__username", "event__title")
     list_filter = ("event",)
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ("id", "username", "email", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_superuser")
